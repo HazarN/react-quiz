66be506a29e3af88bfd6c111f50734316dcd9df7
Hazar <hazarnamdarrr@gmail.com>
2024-10-19 19:02:00 +0300

    feature(core): Project skeleton is structured before develoing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './index.css';
+// Components
+import Header from './components/Header';
+import Main from './components/Main';
 
+// The main app that will be rendered in the browser
 function App() {
   return (
-    <>
-      <h1>React App</h1>
-    </>
+    <div className='app'>
+      <Header />
+
+      <Main>
+        <h2>Welcome to the React Quiz!</h2>
+        <p>
+          This quiz will test your knowledge of React. You will be asked a
+          series of multiple-choice questions. Good luck!
+        </p>
+      </Main>
+    </div>
   );
 }
 
