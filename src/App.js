91e76d107985730b91607b10eea07e4a75c9f82e
Hazar <hazarnamdarrr@gmail.com>
2024-10-20 12:37:21 +0300

    feature(core): Three new components added to start the quiz or showing loading and error messages; useFetch custom hook is removed due to the work of the useEffect's; created a reducer to track all of the state changes in the app

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
-// Custom Hooks
-import { useFetch } from './hooks/useFetch';
+// Libs
+import { useEffect, useReducer } from 'react';
 
 // Components
 import Header from './components/Header';
 import Main from './components/Main';
+import StartScreen from './components/StartScreen';
+import LoadingScreen from './components/LoadingScreen';
+import ErrorMessage from './components/ErrorMessage';
 
 // url of the fake API that's been created with json-server
 const url = 'http://localhost:5000/questions';
 
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataRecevied':
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      };
+    case 'dataFailed':
+      return {
+        ...state,
+        status: 'error',
+      };
+    default:
+      throw new Error('Action does not exist! (HazarN)');
+  }
+}
+const initialState = {
+  questions: [],
+
+  // can be: loading, error, ready, active, finished
+  status: 'loading',
+};
+
 // The main app that will be rendered in the browser
 function App() {
-  useFetch(url);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numOfQuestions = questions.length;
+
+  // fetching on mount
+  useEffect(() => {
+    fetch(url)
+      .then(res => res.json())
+      .then(data => dispatch({ type: 'dataRecevied', payload: data }))
+      .catch(err => dispatch({ type: 'dataFailed' }));
+  }, []);
 
   return (
     <div className='app'>
       <Header />
 
       <Main>
-        <h2>Welcome to the React Quiz!</h2>
-        <p>
-          This quiz will test your knowledge of React. You will be asked a
-          series of multiple-choice questions. Good luck!
-        </p>
+        {status === 'loading' && <LoadingScreen />}
+        {status === 'error' && <ErrorMessage />}
+        {status === 'ready' && <StartScreen numOfQuestions={numOfQuestions} />}
       </Main>
     </div>
   );
